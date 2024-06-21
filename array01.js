550287ebc97f9b26323a802a86aa74e31ebbda50
tweilandt <traiuga@hotmail.com>
2024-06-21 10:33:57 +0200

    Create array01.js

---
+++ array01.js
@@ -0,0 +1,28 @@
+/* var guestList = ["Taís", "Angela", "Jack", "Pam", "James", "Lara", "Jason"];
+var guestName = prompt("What is your name?");
+if (guestList.includes(guestName) === true){
+    alert(`Welcome, ${guestName}!`)
+} else {
+    alert(`Sorry, ${guestName}, try next time!`)
+} */
+
+/*var i = 1;
+while (i <= 100){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0){
+        console.log("Fizz");
+    } else if (i % 5 === 0){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+    i++;
+}*/
+
+/*function whosPaying(names){
+    var n = Math.random();
+    n = n * names.length;
+    n = Math
+}*/
+
