1591a77cd4360caa54fe60be20aabdb97ccb6bc0
tweilandt <traiuga@hotmail.com>
2024-06-24 14:24:34 +0200

    update dom001

--- dom001/index.js
+++ dom001/index.js
@@ -5,3 +5,6 @@ var anchor = document.querySelector("li.list a");
 anchor.style.color = 'red';
 var botao = document.querySelector("button");
 botao.style.backgroundColor = "yellow";
+document.querySelector("h1").classList.toggle("huge");
+document.querySelector("h1").classList.toggle("huge");
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
