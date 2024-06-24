50ada2f312ca24917201bea5ed77243601a6df87
tweilandt <traiuga@hotmail.com>
2024-06-24 14:12:04 +0200

    Update index.js

--- dom001/index.js
+++ dom001/index.js
@@ -1,3 +1,7 @@
 var lista = document.querySelector("ul");
 var nome = lista.lastElementChild;
 nome.innerHTML = "Taís";
+var anchor = document.querySelector("li.list a");
+anchor.style.color = 'red';
+var botao = document.querySelector("button");
+botao.style.backgroundColor = "yellow";
