48604ac8c6c8cfdd133af2c747409aba6242768f
tweilandt <traiuga@hotmail.com>
2024-06-28 12:02:47 +0200

    adicionando eventlisteners com jquery

---
+++ jquery/jquery004/index.js
@@ -0,0 +1,10 @@
+$("h1").click(() => {$("p").html("You <em>got</em> it!")})
+
+$("#itexto").keypress(function(event) {
+    if(event.key === "b" || event.key === "B"){
+        $("#itexto").val(" ");
+        alert("Essa letra nao é permitida. Ele nao!");
+    }
+})
+
+$("h1").on("mouseover", () => {$("h1").css("color", "purple");})
