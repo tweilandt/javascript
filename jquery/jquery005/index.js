6eb359f07f4743932c1a5397664161a73ecabdf4
tweilandt <traiuga@hotmail.com>
2024-06-28 12:22:10 +0200

    remove w jQuery

--- jquery/jquery005/index.js
+++ jquery/jquery005/index.js
@@ -2,3 +2,5 @@ $("p").before("<button>New Button with jQuery</button>");
 $("p").prepend("<input type='text' name='inputJQuery' id='ijqueryinput' placeholder='input qith jQuery'>");
 $("#first-parag").after("<p>New Paragraph </p>");
 $("#first-parag").append("<button>Appeding button with jQuery</button>");
+
+$("button").remove();
