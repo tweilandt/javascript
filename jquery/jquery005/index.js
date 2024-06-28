494e6c769f0a3f38c206fb85cb793d76c431c820
tweilandt <traiuga@hotmail.com>
2024-06-28 13:32:41 +0200

    animate jquery

--- jquery/jquery005/index.js
+++ jquery/jquery005/index.js
@@ -1,6 +1,19 @@
-$("p").before("<button>New Button with jQuery</button>");
-$("p").prepend("<input type='text' name='inputJQuery' id='ijqueryinput' placeholder='input qith jQuery'>");
+$("#first-parag").before("<button>New Button with jQuery</button>");
+$("#first-parag").prepend("<input type='text' name='inputJQuery' id='ijqueryinput' placeholder='input qith jQuery'>");
 $("#first-parag").after("<p>New Paragraph </p>");
 $("#first-parag").append("<button>Appeding button with jQuery</button>");
 
-$("button").remove();
+$("#second-button").click(function(){
+    $("h1").toggle(); 
+    $("#first-parag").fadeToggle();
+    $("#second-parag").slideToggle();
+});
+
+$("#first-button").click(function(){
+    $("h1").animate({opacity: 0.5});
+    $("p").animate({margin: 0});
+})
+
+$("#third-button").click(function () {
+    $("h1").slideUp().slideDown().animate({opacity: 0.5});
+});
