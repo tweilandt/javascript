30aa291445834169347d08958f7cee56a1f8bc91
tweilandt <traiuga@hotmail.com>
2024-07-08 15:34:00 +0200

    module scope

--- Codevolution/NODEJS/nodejs007/index.js
+++ Codevolution/NODEJS/nodejs007/index.js
@@ -1,7 +1,11 @@
-const add = require("./add");
+/*const add = require("./add");
 
 console.log("Olá!");
 
 const sum = add(1,2);
 const sum2 = add(5,4);
-console.log(sum, sum2);
+console.log(sum, sum2);*/
+
+require('./batman');
+require('./superman');
+
