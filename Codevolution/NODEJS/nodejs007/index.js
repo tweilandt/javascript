409890b653f287d4edf2bb883cfbbcb906961b25
tweilandt <traiuga@hotmail.com>
2024-07-08 18:50:28 +0200

    module caching

--- Codevolution/NODEJS/nodejs007/index.js
+++ Codevolution/NODEJS/nodejs007/index.js
@@ -6,6 +6,26 @@ const sum = add(1,2);
 const sum2 = add(5,4);
 console.log(sum, sum2);*/
 
-require('./batman');
-require('./superman');
+/*require('./batman');
+require('./superman');*/
+
+/*const superHero = require('./super-hero')
+
+console.log(superHero.getName());
+
+superHero.setName("Superman");
+console.log(superHero.getName());
+
+const newSuperHero = require('./super-hero');
+console.log(newSuperHero.getName());*/
+
+const SuperHero = require('./super-hero');
+
+const superHero = new  SuperHero('Batman');
+console.log(superHero.getName());
+superHero.setName("Bruce Wayne");
+console.log(superHero.getName());
+
+const newSuperHero = new SuperHero("Batman");
+console.log(newSuperHero.getName());
 
---
+++ Codevolution/NODEJS/nodejs007/super-hero.js
@@ -0,0 +1,17 @@
+class SuperHero{
+    constructor(name){
+        this.name = name;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    setName(name){
+        this.name = name;
+    }
+}
+
+//module.exports = new SuperHero('Batman');
+
+module.exports = SuperHero;
