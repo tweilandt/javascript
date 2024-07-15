458af89b068c5e8bdb3cd83eb18dfb865ab6f2d6
tweilandt <traiuga@hotmail.com>
2024-07-15 19:40:31 +0200

    built-in module path

--- Codevolution/NODEJS/nodejs007/index.js
+++ Codevolution/NODEJS/nodejs007/index.js
@@ -39,5 +39,37 @@ console.log(subtract(1,2));*/
 /*const data = require("./data.json")
 console.log(data)*/
 
-console.log("Hello taís");
+//console.log("Hello taís");
 
+//const path = require("node:path");
+
+const path = require("path");
+
+/*console.log(__filename);
+console.log(__dirname);
+
+console.log(path.basename(__filename));
+console.log(path.basename(__dirname));
+
+console.log(path.extname(__filename));
+console.log(path.extname(__dirname));*/
+
+//console.log(path.parse(__filename));
+
+//console.log(path.format(path.parse(__filename)));
+
+/*console.log(path.isAbsolute(__filename));
+console.log(path.isAbsolute(__dirname));
+console.log(path.isAbsolute("./math.js"));*/
+
+//console.log(path.join("/folder1", "folder2", "index.html"));
+//console.log(path.join("/folder1", "//folder2", "index.html"));
+//console.log(path.join("/folder1", "folder2", "../index.html"));
+
+//console.log(path.join(__dirname, "data.json"));
+
+console.log(path.resolve("folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "index.html"));
+console.log(path.resolve("/folder1", "//folder2", "../index.html"));
+console.log(path.resolve(__dirname, "data.json"));
