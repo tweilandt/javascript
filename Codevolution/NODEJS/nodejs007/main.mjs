69d56a290d31e2c6da5b3207c91e3a435dfaf653
tweilandt <traiuga@hotmail.com>
2024-07-15 16:45:20 +0200

    ESM

---
+++ Codevolution/NODEJS/nodejs007/main.mjs
@@ -0,0 +1,10 @@
+//import add from "./math-esm.mjs"
+
+//import math from "./math-esm.mjs"
+
+//import * as math from "./math-esm.mjs"
+
+import {add, subtract} from "./math-esm.mjs"
+
+console.log(add(1,2));
+console.log(subtract(1,2));
---
+++ Codevolution/NODEJS/nodejs007/math-esm.mjs
@@ -0,0 +1,30 @@
+/*const add = (a,b) => {
+    return a + b;
+};
+
+export default add;*/
+
+/*export default (a,b) =>{
+    return a + b;
+;}*/
+
+/*const add = (a,b) => {
+    return a + b;
+};
+
+const subtract = (a,b) =>{
+    return a - b;
+};
+
+export default {
+    add,
+    subtract,
+}*/
+
+export const add = (a,b) =>{
+    return a + b;
+};
+
+export const subtract = (a,b) => {
+    return a - b;
+};
