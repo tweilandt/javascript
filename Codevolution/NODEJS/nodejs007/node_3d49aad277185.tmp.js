4b09ce46af0fefaf50dda4238d149df6cb2fd96c
tweilandt <traiuga@hotmail.com>
2024-07-20 20:01:26 +0200

    creating a server with node

--- Codevolution/NODEJS/nodejs007/node_3d49aad277185.tmp.js
+++ Codevolution/NODEJS/nodejs007/node_3d49aad277185.tmp.js
@@ -191,11 +191,10 @@ readeableStream.on("data", (chunk) => {
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain"});
+    res.writeHead(200);
     res.end("Hello World!");
-    console.log(req, res);
 });
 
 server.listen(3000, () => {
     console.log("Server is running on port 3000");
-});
+})
--- Codevolution/NODEJS/nodejs007/node_e7ebc80b8754b.tmp.js
+++ Codevolution/NODEJS/nodejs007/node_e7ebc80b8754b.tmp.js
@@ -193,7 +193,6 @@ const http = require("node:http");
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain"});
     res.end("Hello World!");
-    console.log(req, res);
 });
 
 server.listen(3000, () => {
