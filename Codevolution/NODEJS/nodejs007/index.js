4b53aee1abf9ff654da77b9c75cf5fe7445e0652
tweilandt <traiuga@hotmail.com>
2024-07-21 09:01:00 +0200

    html response

--- Codevolution/NODEJS/nodejs007/index.js
+++ Codevolution/NODEJS/nodejs007/index.js
@@ -189,11 +189,19 @@ readeableStream.on("data", (chunk) => {
 });*/
 
 const http = require("node:http");
+const fs = require("node:fs");
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain"});
-    res.end("Hello World!");
-    console.log(req, res);
+
+    /*const superHero = {
+        firstName: "Bruce",
+        lastName: "Wayne"
+    }*/
+    res.writeHead(200, { "Content-Type": "text/html"});
+    fs.createReadStream(__dirname + "/index.html").pipe(res);
+    //const html = fs.readFileSync("./index.html", "utf-8");
+    //res.end(html);
+    //res.end("<h1>Voce consegue! Maravilhosa!</h1>");
 });
 
 server.listen(3000, () => {
