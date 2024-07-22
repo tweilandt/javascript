bd86e32dcb0b3b189b3ee5370d1aca406a8d1dc6
tweilandt <traiuga@hotmail.com>
2024-07-22 18:41:52 +0200

    network i/o

--- Codevolution/NODEJS/nodejs007/thread-pool.js
+++ Codevolution/NODEJS/nodejs007/thread-pool.js
@@ -1,15 +1,24 @@
+const https = require("node:https");
 
-const crypto = require("node:crypto");
+//const crypto = require("node:crypto");
 
-process.env.UV_THREADPOOL_SIZE = 16;
-const MAX_CALLS = 16;
+//process.env.UV_THREADPOOL_SIZE = 16;
+const MAX_CALLS = 1;
 
 const start = Date.now();
 
 for (let i = 0; i < MAX_CALLS; i++){
-    crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () =>{
+    https
+        .request("https://www.google.com", (res) =>{
+            res.on("data", () =>{});
+            res.on("end", () =>{
+                console.log(`REquest: ${ i + 1 }`, Date.now() - start);
+            });
+        })
+        .end();
+    /*crypto.pbkdf2("password", "salt", 100000, 512, "sha512", () =>{
         console.log(`Has: ${i + 1}`, Date.now() - start);
-    });
+    });*/
 }
 
 /*const start = Date.now();
