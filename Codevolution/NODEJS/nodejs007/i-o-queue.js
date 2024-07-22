a1e979d4dde466851145f5a1f55913b54e26a34f
tweilandt <traiuga@hotmail.com>
2024-07-22 22:04:49 +0200

    i/o polling

--- Codevolution/NODEJS/nodejs007/i-o-queue.js
+++ Codevolution/NODEJS/nodejs007/i-o-queue.js
@@ -9,5 +9,6 @@ fs.readFile(__filename, () => {
 process.nextTick(() => console.log("this is process.nextTick 1"));
 Promise.resolve().then(() => console.log("this is promise.resolve 1"));
 setTimeout(() => console.log("this is setTimeout 1"), 0);
+setImmediate(() => console.log("this is setImmediate 1"));
 
 for(let i = 0; i < 20000; i++);
