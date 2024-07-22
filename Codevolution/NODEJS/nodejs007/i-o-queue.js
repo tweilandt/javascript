9d1f67ff68372a5d5a8a6496cdbfdb6c74eee344
tweilandt <traiuga@hotmail.com>
2024-07-22 21:46:39 +0200

    i/o queue exp. 2

--- Codevolution/NODEJS/nodejs007/i-o-queue.js
+++ Codevolution/NODEJS/nodejs007/i-o-queue.js
@@ -1,8 +1,10 @@
 const fs = require("node:fs");
 
+setTimeout(() => console.log("this is setTimeout 1"), 0)
+
 fs.readFile(__filename, () => {
     console.log("this is readFile 1");
 });
 
-process.nextTick(() => console.log("this is process.nextTick 1"));
-Promise.resolve().then(() => console.log("this is promise.resolve 1"));
+/*process.nextTick(() => console.log("this is process.nextTick 1"));
+Promise.resolve().then(() => console.log("this is promise.resolve 1"));*/
