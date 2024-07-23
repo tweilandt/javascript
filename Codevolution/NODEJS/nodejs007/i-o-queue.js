30133fa2106ed30faabc4c70558ea816c8aecb2d
tweilandt <traiuga@hotmail.com>
2024-07-23 09:20:26 +0200

    check queue exp. 1

--- Codevolution/NODEJS/nodejs007/i-o-queue.js
+++ Codevolution/NODEJS/nodejs007/i-o-queue.js
@@ -4,11 +4,12 @@ const fs = require("node:fs");
 
 fs.readFile(__filename, () => {
     console.log("this is readFile 1");
+    setImmediate(() => console.log("this is inner setImmediate 1 inside readFile"));
 });
 
 process.nextTick(() => console.log("this is process.nextTick 1"));
 Promise.resolve().then(() => console.log("this is promise.resolve 1"));
 setTimeout(() => console.log("this is setTimeout 1"), 0);
-setImmediate(() => console.log("this is setImmediate 1"));
+
 
 for(let i = 0; i < 20000; i++);
