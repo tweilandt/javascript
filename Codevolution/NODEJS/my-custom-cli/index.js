91d7997b32882c715bacdf9b2f63a776799b6983
tweilandt <traiuga@hotmail.com>
2024-08-01 11:20:05 +0200

    CLI Options

---
+++ Codevolution/NODEJS/my-custom-cli/index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+//console.log("tweilandt pokedex");
+
+const yargs = require("yargs");
+const { argv } = yargs(process.argv);
+
+const printFiveMoves = async (pokemonName) => {
+    const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+
+    const pokemon = await response.json();
+    const moves = pokemon.moves.map (({ move }) => move.name);
+    console.log(moves.slice(0,5));
+}
+
+printFiveMoves(argv.pokemon);
